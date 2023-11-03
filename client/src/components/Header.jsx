da6b102519aac424d7702de35db6266378e596aa
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 <vivekratr@gmail.com>
2023-11-03 19:26:27 +0530

    Update Header and Mainpage components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -115,3 +115,70 @@ export default function Header() {
 
 
 
+// import Logo from "../assets/logo.png";
+// import Logo2 from "../assets/logo2.png";
+// import React, { useState } from "react";
+// import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+// export default function Header() {
+//   const [isNavOpen, setNavOpen] = useState(false);
+
+//   return (
+//     <div className="flex flex-col lg:flex-row lg:items-center justify-between lg:mb-8 max-w-full max-h-24">
+//       <div className="my-5 mx-6 bg-none relative w-full lg:w-[16rem] h-24 flex flex-row items-center justify-start gap-2">
+//         <img
+//           src={Logo}
+//           alt="Logo"
+//           className="w-16 h-16 object-cover"
+//         />
+//         <img
+//           src={Logo2}
+//           alt="Logo2"
+//           className="w-32 h-10 object-cover"
+//         />
+//       </div>
+
+//       {/* Responsive Nav Toggle Button */}
+//       <div className="lg:hidden ml-4">
+//         <button
+//           className="w-10 h-10 flex items-center justify-center cursor-pointer"
+//           onClick={() => setNavOpen(!isNavOpen)}
+//         >
+//           {isNavOpen ? (
+//             <AiOutlineClose size={24} />
+//           ) : (
+//             <AiOutlineMenu size={24} />
+//           )}
+//         </button>
+//       </div>
+
+//       {/* Responsive Navigation Menu */}
+//       {isNavOpen && (
+//         <div className="lg:hidden ml-4 max-w-full">
+//           <p className="cursor-pointer my-1">About</p>
+//           <p className="cursor-pointer my-1">Gallery</p>
+//           <p className="cursor-pointer my-1">Amenities</p>
+//           <p className="cursor-pointer my-1">Floor Plans</p>
+//           <p className="cursor-pointer my-1">Virtual Tours</p>
+//           <p className="cursor-pointer my-1">Our Partners</p>
+//           <button className="cursor-pointer w-32 h-16 my-4 rounded-full bg-yellow-600 flex items-center justify-center">
+//             <p className="text-white">Enquire Now</p>
+//           </button>
+//         </div>)
+//       }
+
+//       {/* Original Navigation for Desktop */}
+//       <div className="hidden lg:flex lg:flex-row my-5 ml-4 gap-12 max-w-full lg:max-h-24 lg:items-center font-dm-serif-display">
+//         <p className="cursor-pointer">About</p>
+//         <p className="cursor-pointer">Gallery</p>
+//         <p className="cursor-pointer">Amenities</p>
+//         <p className="cursor-pointer">Floor Plans</p>
+//         <p className="cursor-pointer">Virtual Tours</p>
+//         <p className="cursor-pointer">Our Partners</p>
+//         <button className="w-32 h-16 mx-4 rounded-full bg-yellow-600 flex items-center justify-center">
+//           <p className="text-white">Enquire Now</p>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
