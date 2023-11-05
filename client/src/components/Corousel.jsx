169cbe41135e71d1d3d78dd30c5885e11be11d3d
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 <vivekratr@gmail.com>
2023-11-05 09:39:06 +0530

    Add react-responsive-carousel package and custom navigation to Corousel component

--- client/src/components/Corousel.jsx
+++ client/src/components/Corousel.jsx
@@ -1,23 +1,42 @@
 import { Carousel } from "@material-tailwind/react";
- 
-export function CarouselDefault() {
+
+export default function CarouselCustomNavigation() {
   return (
-    <Carousel className="rounded-xl">
+    <Carousel
+      className="rounded-3xl aspect-square overflow-hidden h-[100%] w-[100%] lg:w-[90%] relative z-10"
+      navigation={({ setActiveIndex, activeIndex, length }) => (
+        <div className="relative bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+          {new Array(length).fill("").map((_, i) => (
+            <span
+              key={i}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+              }`}
+              onClick={() => setActiveIndex(i)}
+            />
+          ))}
+        </div>
+      )}
+    >
       <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+        src="https://cdn.discordapp.com/attachments/1096324843877703713/1170447206298435695/image.png?ex=655912e7&is=65469de7&hm=ee5587fc85aa92f10da1c39b6b8851635ce4f2e1b605eff6730d03ba4b05a528&"
         alt="image 1"
-        className="h-full w-full object-cover"
+        className="object-cover "
+        style={{ objectFit: "cover" }} // Add this line
       />
-      <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="image 2"
-        className="h-full w-full object-cover"
+       <img
+        src="https://cdn.discordapp.com/attachments/1096324843877703713/1170449806326509629/image.png?ex=65591553&is=6546a053&hm=39a63eb208dc06999a7b2deb3b330813fbe72caa16364ab341a2e7ff40b6f350&"
+        alt="image 1"
+        className="object-cover overflow-y-hidden"
+        style={{ objectFit: "cover" }} // Add this line
       />
-      <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="image 3"
-        className="h-full w-full object-cover"
+       <img
+        src="https://cdn.discordapp.com/attachments/1096324843877703713/1170452120047210556/WhatsApp_Image_2023-11-02_at_22.53.00_c990782c.jpg?ex=6559177b&is=6546a27b&hm=3fe78ed4e0a0ef942b5f23fb759c76b19db71e00e0ae267f8ab9369b7445b2b4&"
+        alt="image 1"
+        className="object-cover overflow-y-hidden"
+        style={{ objectFit: "cover" }} // Add this line
       />
+      {/* Add similar style for other images if necessary */}
     </Carousel>
   );
 }
--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import { Button } from "@material-tailwind/react";
 import React, { useState } from "react";
-
+import Corousel from "./Corousel";
 
 export default function Mainpage() {
   const backgroundImageUrl = "https://cdn.discordapp.com/attachments/1096324843877703713/1169889030075326545/image_12.png?ex=65570b10&is=65449610&hm=15386f2762e0011dbe26eae3f532949505320da5aab46cec39e877b613a1acd4&";
@@ -24,11 +24,11 @@ export default function Mainpage() {
       <Header />
       {/* <Button>Button</Button>; */}
       {/* <h1 className="color-white">Mainpage</h1> */}
-    <div className="relative">
+    <div className="  w-full flex  flex-wrap  justify-around">
 
-      <div className="flex flex-row flex-wrap gap-3 justify-evenly w-[100%] lg:w-[50%] ">
-
-        <div style={{borderRadius:"32px"}} className="relative  p-7 bg-white w-[60%] lg:max-w-[60%] h-[31.19rem]  text-left text-[1.75rem] text-yellow-700 font-dm-serif-displays">
+      <div className=" ml-0  w-[100%] lg:w-[40%] flex justify-around ">
+        {/* right side content */}
+              <div style={{borderRadius:"32px"}} className="  relative  p-7 bg-white w-[60%] lg:w-[65%] lg:ml-14 h-[31.19rem]  text-left text-[1.75rem] text-yellow-700 font-dm-serif-displays">
         <div className="h-fit text-[1.7rem] font-dm-serif-display text-yellow-400 text-left">CASCADE AVENUE</div>
         <div className="h-fit text-[0.8rem] font-dm-serif-display text-yellow-600 text-left">KASARVADAVALI, GB ROAD, THANE[W] - 400615</div>
         <div className="h-fit mt-7  text-[2rem] font-dm-serif-display text-red-600 text-left">Largest 1 BHK Of THANE</div>
@@ -58,15 +58,22 @@ export default function Mainpage() {
       <div className="absolute text-[1.75rem] font-dm-serif-display text-yellow-700 text-left mt-6">
         {is1BHKSelected ? <div className="relative w-[80%]  text-[0.8em] font-dm-serif-display text-yellow-700 text-center">52.47* Lakhs Starting Price</div> :<div className="relative w-[80%] text-[0.8em] font-dm-serif-display text-yellow-700 text-center">XX* Lakhs Starting Price</div>}
       </div>
-      <div className="relative xs:left-[9%] left-[30%] top-[5rem] rounded-[46px] bg-yellow-600 xs:min-w-[80%] min-w-[39%] max-w-[35%] h-[3.25rem] overflow-hidden text-justify text-[0.9rem] text-white font-dm-serif-display">
-<div className="relative top-[0.8rem] left-[2.8rem]">Enquire Now</div>
+      <div className="relative xs:left-[9%] left-[30%] top-[5rem] rounded-[46px] bg-yellow-600 xs:min-w-[80%]  min-w-[39%] max-w-[35%] h-[3.25rem] overflow-hidden text-justify text-[0.9rem] text-white font-dm-serif-display">
+<div className="relative top-[0.8rem] lg:left-[10rem] left-[4.8rem]">Enquire Now</div>
 </div>
 
 
 
         </div>
 
+
+
       </div>
+        {/*left side corousel */}
+        <div className=" h-[78%] w-[100%] aspect-square  overflow-y-hidden lg:w-[60%]">
+
+        <Corousel />
+        </div>
     </div>
     </div>
   );
