fdf18409289df9fed6a563095d5717ee9b41d391
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 <vivekratr@gmail.com>
2023-11-05 09:55:43 +0530

    Add background image to Mainpage component

--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -10,6 +10,7 @@ export default function Mainpage() {
     backgroundImage: `url(${backgroundImageUrl})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
+    
     height: "100vh", 
   };
 
