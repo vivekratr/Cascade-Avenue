24680bb10da53cde38571a18ff15bec729cce3cf
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 <vivekratr@gmail.com>
2023-11-11 03:20:45 +0530

    Refactor Modal component styles.

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -4,9 +4,10 @@ const Modal = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
   return (
     <div className="fixed z-[999] inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <div className="w-[600px] h-[400px] bg-white flex flex-col">
-        <div className="relative bg-peachpuff w-[632px] max-w-[632px] h-[9.25rem] overflow-hidden text-justify text-[1.75rem] text-yellow-700 font-dm-serif-display">
-          <div className="absolute top-[0.94rem] left-[16.38rem]">
+      <div className="max-w-[600px] h-[400px] bg-white flex flex-col">
+        <div className="bg-white  shadow-lg p-0">
+        <div className="relative bg-peachpuff w-full max-w-[632px] h-[9.25rem] overflow-hidden text-justify text-[1.75rem] text-yellow-700 font-dm-serif-display">
+          <div className="mx-auto w-fit h-fit my-auto">
             Enquire Here
           </div>
           <img
@@ -14,8 +15,7 @@ const Modal = ({ isVisible, onClose }) => {
             alt=""
             src="https://cdn.discordapp.com/attachments/1096324843877703713/1172647855064891412/image.png?ex=6561146b&is=654e9f6b&hm=1c4fe06c279649c348b25e08c06e811c425ebe93388a4ffee09fdfafdc22bacb&"
           />
-        </div>
-        <div className="bg-white  shadow-lg p-4">Modal</div>
+        </div>Modal</div>
       </div>
     </div>
   );
