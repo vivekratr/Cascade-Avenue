06727bebb1314b31cc20a2570b74a14207120f5c
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 <vivekratr@gmail.com>
2023-11-11 03:17:45 +0530

    Refactor Modal component to improve readability and add onClose functionality

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Modal = ({isVisible}) => {
-    if (!isVisible) return null;
+const Modal = ({ isVisible, onClose }) => {
+  if (!isVisible) return null;
   return (
     <div className="fixed z-[999] inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <div className="w-[600px] h-[400px] bg-white">
-        <div className="relative bg-peachpuff w-[630px] h-[4.25rem] overflow-hidden text-justify text-[1.75rem] text-yellow-700 font-dm-serif-display">
+      <div className="w-[600px] h-[400px] bg-white flex flex-col">
+        <div className="relative bg-peachpuff w-[632px] max-w-[632px] h-[9.25rem] overflow-hidden text-justify text-[1.75rem] text-yellow-700 font-dm-serif-display">
           <div className="absolute top-[0.94rem] left-[16.38rem]">
             Enquire Here
           </div>
@@ -15,7 +15,7 @@ const Modal = ({isVisible}) => {
             src="https://cdn.discordapp.com/attachments/1096324843877703713/1172647855064891412/image.png?ex=6561146b&is=654e9f6b&hm=1c4fe06c279649c348b25e08c06e811c425ebe93388a4ffee09fdfafdc22bacb&"
           />
         </div>
-        <div className="bg-white rounded-lg shadow-lg p-4">Modal</div>
+        <div className="bg-white  shadow-lg p-4">Modal</div>
       </div>
     </div>
   );
