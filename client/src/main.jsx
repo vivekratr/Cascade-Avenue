5b79e5474048fe50aadad9057a3e5395bb6e27a2
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2023-11-13 20:52:22 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <ThemeProvider>
+   <ThemeProvider> 
       <App />
     </ThemeProvider>
   </React.StrictMode>,
